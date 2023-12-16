a0b07418d9b496cfb1a2215d1ceac5197b8047e7
Gurpartap <gurpartap6125@gmail.com>
2023-12-16 14:27:49 +0530

    added the profile page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Nav_Bar from './components/Nav_bar';
 import Container from './components/Container';
+import Profile from './profile_components/profile';
+
 function App() {
   return (
     <div className="App">
       <>
-      <Nav_Bar/>
+      <Profile/>
+      {/* <Nav_Bar/>
       <div style={{width:"100%",display:"flex",justifyContent:"center"}}>
-      <Container/>
-      </div>
+      <Container/> */}
+      {/* </div> */}
       </>
     </div>
   );
---
+++ src/profile_components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      hi
+
+    </div>
+  )
+}
+
+export default Profile
